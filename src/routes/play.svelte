3e10394bf1f4c585d1622b2f20e25134cd30920d
wolfi <wolfi@disroot.org>
2022-04-15 01:06:31 +0200

    add itch widget

--- src/routes/play.svelte
+++ src/routes/play.svelte
@@ -18,21 +18,24 @@
 	<title>Geocats - Game</title>
 	<meta name="description" content="A Retro-style 2D Action Platformer - Our adventure story takes the player through the world of GeoTerra, a place of both breathtaking beauty and chilling dystopian environments." />
 </svelte:head>
-<section>
-	<center>
-	</center>
-</section>
+
 <div id="game" class="black">
 	<div class="content">
-      <iframe frameborder="0" src="https://itch.io/embed-upload/5611548?color=000000" allowfullscreen="" width="640" height="480"><a href="https://nonaco.itch.io/geocats">Play Geocats on itch.io</a></iframe>
+	<iframe frameborder="0" src="https://itch.io/embed-upload/5611548?color=000000" allowfullscreen="" width="640" height="480"><a href="https://nonaco.itch.io/geocats">Play Geocats on itch.io</a></iframe>
 	</div>
-</div>
 
+</div>
+<section>
+	<center>
+	<iframe frameborder="0" src="https://itch.io/embed/1484216?bg_color=0d0f15&amp;fg_color=d9d737&amp;link_color=b933a2&amp;border_color=333333" width="552" height="167"><a href="https://nonaco.itch.io/geocats">Geocats by Nonaco</a></iframe>
+
+	</center>
+</section>
 <style>
 	.black {
 		background-color: black;
 		height: 110vh;
-		margin-top: 2rem;
+		margin-top: 12rem;
 		display: flex;
 		justify-content: center;
 		align-items: center;
@@ -45,14 +48,9 @@
 		margin: auto;
 		text-align: center;
 		max-width: 760px;
-		max-height: 570px;
 	}
-	.content iframe {
-		width: 90vw;
-		height: 80vh;
-		border: none;
-		max-width: 760px;
-		max-height: 570px;
+	section {
+		margin-bottom: 10rem;
 	}
 
 </style>
